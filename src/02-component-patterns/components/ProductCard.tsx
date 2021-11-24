a06b73f3a7dd94bfe6210411c612681bd99652e9
D3vAya <j.ayala.higuera@gmail.com>
2021-11-24 01:03:42 -0300

    component compund

---
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -0,0 +1,27 @@
+import styles from "../styles/styles.module.css";
+
+import { useProduct } from "../hooks/useProduct";
+import { createContext } from "react";
+import {
+  ProductContextProps,
+  ProductCardProps,
+} from "../interfaces/interfaces";
+
+export const productContext = createContext({} as ProductContextProps);
+const { Provider } = productContext;
+
+export const ProductCard = ({ children, product }: ProductCardProps) => {
+  const { counter, increaseBy } = useProduct(0);
+
+  return (
+    <Provider
+      value={{
+        counter,
+        increaseBy,
+        product,
+      }}
+    >
+      <div className={styles.productCard}>{children}</div>
+    </Provider>
+  );
+};
